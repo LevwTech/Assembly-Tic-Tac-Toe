7eab56709792ddd7cff9b83d7aa9ba1165c8600c
Abdelrahman Mostafa <Abdelraahmanmostafa@gmail.com>
2022-12-14 16:45:37 +0200

    Update game.c

--- game.c
+++ game.c
@@ -1,57 +1,65 @@
-#include <stdio.h>
-#include <stdbool.h>
-bool checkWin(char board[3][3], char player);
-void printBoard(char board[3][3]);
-int main()
-{
-    printf("Welcome to Tic Tac Toe! \n");
-    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-    for (int k = 0; k < 9; k++)
-    {
-        printBoard(board);
-        char position, player = k % 2 == 0 ? 'x' : 'o';
-        printf("Enter the Position Number, Player %c Turn \n", player);
-        scanf(" %c", &position);
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                if (board[i][j] == position)
-                {
-                    board[i][j] = player;
-                }
-            }
-        }
-        if (checkWin(board, player))
-        {
-            printBoard(board);
-            printf("%c won! \n", player);
-            return 0;
-        }
-    }
-    printf("Draw! \n");
-    return 0;
-}
-bool checkWin(char board[3][3], char player)
-{
-    bool win;
-    for (int i = 0; i < 3; i++)
-    {
-        win = win || (board[i][0] == player && board[i][1] == player && board[i][2] == player);
-        win = win || (board[0][i] == player && board[1][i] == player && board[2][i] == player);
-    }
-    win = win || (board[0][0] == player && board[1][1] == player && board[2][2] == player);
-    win = win || (board[0][2] == player && board[1][1] == player && board[2][0] == player);
-    return win;
-}
-void printBoard(char board[3][3])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }
-}
+name "Tic Tac Toe"
+org 100h
+ 
+ .DATA
+ 
+    grid db '1','2','3'  ; defining the 2D array grid
+         db '4','5','6'
+         db '7','8','9'
+    
+    welcomeMsg db 'Welcome to Tic Tac Toe! $' ; terminator is the dollar sign   
+    inputMsg db 'Enter Position Number, Player Turn is: $'
+    
+ .CODE
+    main:
+        lea dx, welcomeMsg  ; load offset of welcome msg into dl.
+        mov ah, 9           ; print string interupt is 9  
+        int 21h
+        call printNewLine
+          
+        ;mov cx,9  
+        ;x: 
+           call printGrid
+        ;loop x 
+        
+    
+     
+        mov     ah, 0    ; wait for any key interupt
+        int     16h      
+        ret              ; final return in main that returns to operating system 
+    
+    
+        printGrid:
+            mov bx,0
+            mov cx,3
+            x1:
+                call printNewLine 
+                push cx          ; push cx to stack to maintain the index of the first loop
+                mov cx, 3
+                x2:
+                    mov dl, grid[bx]  
+                    sub al, 30h  ; to convert ascii to char 
+                    mov ah, 2h   ; print character interupt is 2
+                    int 21h
+                    call printSpace              
+                    inc bx       ; increment bx to move the index the next element (from 0 to 8)
+                loop x2
+                pop cx           ; pop cx from stack to get the index of the first loop             
+            loop x1                        
+        ret 
+        
+        printNewLine:
+            mov dl, 0ah      ; new line in ascii 
+            mov ah, 2        
+            int 21h
+            mov dl, 13       ; carriage return in ascii 
+            mov ah, 2        
+            int 21h
+        ret 
+        
+        printSpace:
+            mov dl, 32        ; space in ascii 
+            mov ah, 2         
+            int 21h
+        ret    
+    end main                     
